47e43c64b3c7096456243888f962e16d5b714045
erika <ec.silvaalvarado@ugto.mx>
2022-08-11 14:34:19 -0500

    add method post to decorator /load_options

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def catchUser():
     # renderizamos la plantilla "index.html"
     return render_template('index.html')
 
-@app.route("/load_options")
+@app.route("/load_options", methods=['POST', 'GET'])
 def listingTables():
     engine = create_engine("mysql+pymysql://b9b5c80ea73822:f09bb1f5@us-cdbr-east-06.cleardb.net/heroku_a5313fa6d44ab5f")
     #engine = create_engine("mysql+pymysql://esilva:Cr1st0_R3y@localhost/MZI_SCF_fatt")
